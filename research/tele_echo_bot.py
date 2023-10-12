ad6ee54dff041d1ba071751936e2c3cebbe7fa15
tintu <ttuthms1992@gmail.com>
2023-10-12 19:59:01 +0530

    update

--- research/tele_echo_bot.py
+++ research/tele_echo_bot.py
@@ -7,4 +7,22 @@ import sys
 
 load_dotenv()
 API_TOKEN = os.getenv("TOKEN")
-print(API_TOKEN)
+# print(API_TOKEN)
+
+# congigure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def command_start_handler(message: types.Message):
+    """
+    This handler receives messages with `/start` or  `/help `command
+    """
+    await message.reply("Hi\nI am Tele Bot!\Created by Bappy. How can i assist you?")
+    
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
+    
