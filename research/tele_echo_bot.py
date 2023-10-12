63a9f26f1eeaac6108285e6ec22ace50c8d98dce
tintu <ttuthms1992@gmail.com>
2023-10-12 16:42:59 +0530

    updated

---
+++ research/tele_echo_bot.py
@@ -0,0 +1,10 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+import os
+import openai
+import sys
+
+load_dotenv()
+API_TOKEN = os.getenv("TOKEN")
+print(API_TOKEN)
